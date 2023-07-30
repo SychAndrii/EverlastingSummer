f88fd6522704fbf8d31a70f0b46cac438b04b506
SychAndrii <97425977+SychAndrii@users.noreply.github.com>
2023-07-30 14:44:24 -0400

    Generated xml docs file.

--- ConsoleTesting/Program.cs
+++ ConsoleTesting/Program.cs
@@ -12,31 +12,7 @@ namespace Program
     {
         public static void Main(string[] args)
         {
-            using (ESContext context = new ESContext()) 
-            {
-                context.Database.EnsureDeleted();
-                context.Database.EnsureCreated();
-                Ending end1 = new Ending("Ending 1");
-                Ending end2 = new Ending("Ending 2");
-                context.Endings.AddRange(end1, end2);
 
-                EndingPointsCondition endingPointsCondition = new EndingPointsCondition();
-                endingPointsCondition.Ending = end1;
-                endingPointsCondition.PointsRequired = 4;
-                context.EndingPointsConditions.Add(endingPointsCondition);
-
-                context.SaveChanges();
-            }
-
-            using (ESContext context = new ESContext())
-            {
-                var endingPointsEnding = context.EndingPointsConditions.Include(c => c.Ending).FirstOrDefault();
-                if(endingPointsEnding != null )
-                {
-                    Console.WriteLine(endingPointsEnding.Ending);
-                    Console.WriteLine(endingPointsEnding.PointsRequired);
-                }
-            }
         }
     }
 }

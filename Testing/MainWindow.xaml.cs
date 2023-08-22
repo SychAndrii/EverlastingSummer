12bb309581a617442fe7191e93e6f5fab00c3b04
SychAndrii <97425977+SychAndrii@users.noreply.github.com>
2023-08-22 17:11:32 -0400

    Added drag-and-drop functionality for story designer.

--- Testing/MainWindow.xaml.cs
+++ Testing/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace Testing
 {
@@ -23,6 +26,33 @@ namespace Testing
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void canvas_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetData("Element") is UIElement elem && sender is Canvas canvas)
+            {
+                Point dropPosition = e.GetPosition(canvas);
+
+                Canvas.SetLeft(elem, dropPosition.X);
+                Canvas.SetTop(elem, dropPosition.Y);
+                if(canvas.Children.Contains(elem))
+                    canvas.Children.Remove(elem);
+                canvas.Children.Add(elem);
+            }
+        }
+
+        private void canvas_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetData(typeof(UIElement)) is UIElement elem && sender is Canvas canvas)
+            {
+                Point dropPosition = e.GetPosition(canvas);
+
+                Canvas.SetLeft(elem, dropPosition.X);
+                Canvas.SetTop(elem, dropPosition.Y);
+                if (!canvas.Children.Contains(elem))
+                    canvas.Children.Add(elem);
+            }
         }
     }
 }

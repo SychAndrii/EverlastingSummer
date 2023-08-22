b4fea05b2edeab16fecca0cbcbb454a24c7decf6
SychAndrii <97425977+SychAndrii@users.noreply.github.com>
2023-08-22 12:55:42 -0400

    Added Elements view.

--- Testing/App.xaml.cs
+++ Testing/App.xaml.cs
@@ -6,10 +6,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace EverlastingSummer
+namespace Testing
 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
     public partial class App : Application
     {
-
     }
 }
